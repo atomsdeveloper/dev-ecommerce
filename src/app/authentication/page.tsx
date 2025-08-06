f74a31a48ac131d2458630c3918e6a029f6c168f
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-06 20:03:16 -0300

    fix: css page

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,28 +1,34 @@
 // UI Components
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-// Components
+// Components authentication
 import SignInForm from "./components/sign-in-form";
 import SignUpForm from "./components/sign-up-form";
 
+// Components Common
+import Header from "../../components/common/header";
+
 const Authentication = async () => {
   return (
-    <div className="flex w-full max-w-sm flex-col gap-5 p-5">
-      <Tabs defaultValue="sign-in" className="w-[400px]">
-        <TabsList>
-          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
-          <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
-        </TabsList>
-        {/* SIGN IN */}
-        <TabsContent value="sign-in">
-          <SignInForm />
-        </TabsContent>
-        {/* SIGN UP */}
-        <TabsContent value="sign-up">
-          <SignUpForm />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <>
+      <Header />
+      <div className="flex w-full flex-col gap-5 p-5">
+        <Tabs defaultValue="sign-in" className="w-full">
+          <TabsList>
+            <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+            <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
+          </TabsList>
+          {/* SIGN IN */}
+          <TabsContent value="sign-in" className="w-full">
+            <SignInForm />
+          </TabsContent>
+          {/* SIGN UP */}
+          <TabsContent value="sign-up" className="w-full">
+            <SignUpForm />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 };
 
