a8ddc442b601a44a2310ac82bfd808f9e3e6cc8d
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-06 20:03:41 -0300

    feta: created componente to render products

---
+++ src/components/common/product-item.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+// Database
+import { productTable, productVariantTable } from "@/db/schema";
+import { formatCentsToBRL } from "@/helpers/money";
+
+// Next
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProducItemProps {
+  product: typeof productTable.$inferSelect & {
+    variants: (typeof productVariantTable.$inferSelect)[];
+  };
+}
+
+const ProductList = ({ product }: ProducItemProps) => {
+  const firstVariant = product.variants[0];
+
+  const extractUrl = (raw: string): string => {
+    const match = raw.match(/https?:\/\/[^"}]+/);
+    return match ? match[0] : "";
+  };
+
+  const parsedImageUrl: string = extractUrl(firstVariant.imageUrl);
+  console.log("parseImageUrl:", parsedImageUrl);
+
+  return (
+    <Link href="/" className="flex flex-col gap-4">
+      <Image
+        src={parsedImageUrl}
+        alt={firstVariant.slug}
+        height={150}
+        width={150}
+        className="rounded-2xl"
+      />
+      <div className="flex max-w-[150px] flex-col gap-1">
+        <p className="truncate text-sm font-medium"> {product.name} </p>
+        <p className="text-muted-foreground truncate text-xs font-medium">
+          {product.description}
+        </p>
+        <p className="truncate text-sm font-semibold">
+          {formatCentsToBRL(firstVariant.priceInCents)}
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductList;
