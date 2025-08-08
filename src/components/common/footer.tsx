6848897342117792b97114844fc2aea5c71cd9cc
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-07 23:45:01 -0300

    fix:  footer

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="bg-accent w-full gap-1 p-8">
-      <p className="text-sm font-medium">@ 2025 Copyright CLOUTHING</p>
+      <p className="text-sm font-medium">@ 2025 Copyright CLOTHING</p>
       <p className="text-muted-foreground text-xs font-medium">
         Todos os direitos reservados.
       </p>
